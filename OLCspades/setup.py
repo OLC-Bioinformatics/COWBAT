6ca137a0886f914867eb758b123c546baa4ff719
adamkoziol <adam.koziol@inspection.gc.ca>
2017-08-30 11:02:20 -0400

    Cleaning up files - this repo will only contain wrapper scripts to run the pipeline. The accessory scripts will be located in the OLCTools python package

--- OLCspades/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='olctools',
-    version='0.1.0',
-    description='Things used in the assembly pipeline in package format.',
-    packages=find_packages('.')
-)

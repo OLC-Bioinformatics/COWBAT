a4493aa9b247f3d48a8f4ecd200a2c97dd2a0718
adamkoziol <adam.koziol@inspection.gc.ca>
2017-12-21 11:09:16 -0500

    Cleaned up imports with typingclasses now in OLCTools

--- assembly_pipeline.py
+++ assembly_pipeline.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-try:
-    from .typingclasses import Quality, GeneSippr, Resistance, Prophages, Plasmids, Univec, Virulence
-except ImportError:
-    from typingclasses import Quality, GeneSippr, Resistance, Prophages, Plasmids, Univec, Virulence
+from spadespipeline.typingclasses import Quality, GeneSippr, Resistance, Prophages, Plasmids, Univec, Virulence
 from accessoryFunctions.accessoryFunctions import MetadataObject, GenObject, printtime, make_path
 from sixteenS.sixteens_full import SixteenS as SixteensFull
 import spadespipeline.metadataprinter as metadataprinter

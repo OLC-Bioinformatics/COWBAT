acbaf32e98048dcbb4583391a3a5ca2b0a515554
adamkoziol <adam.koziol@inspection.gc.ca>
2018-10-19 11:34:14 -0400

    Attempting to fix environment solving issue

--- validation/run_validation.sh
+++ validation/run_validation.sh
@@ -14,4 +14,4 @@
 #SBATCH -e /mnt/nas2/redmine/bio_requests/12430/slurm_logs/job_%j.err
 
 # Your code goes here. 
-docker run -u ubuntu -i -v /mnt/nas2:/mnt/nas2 --name cowbat --rm cowbat:latest /bin/bash -c "source activate cowbat && python3 assembly_pipeline.py -s /mnt/nas2/redmine/bio_requests/12430/fastqs -r /mnt/nas2/databases/assemblydatabases/0.3.2"
+docker run -u ubuntu -i -v /mnt/nas2:/mnt/nas2 --name cowbat --rm cowbat:latest /bin/bash -c "source activate cowbat && python3 assembly_pipeline.py -s /mnt/nas2/redmine/bio_requests/12430/fastqs -r /mnt/nas2/databases/assemblydatabases/0.3.4"
